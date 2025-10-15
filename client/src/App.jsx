7cf60c20e56ffb7a8741d10a88cb74c4aa3b3514
Shivam67Code <evilsniper64@gmail.com>
2025-10-15 12:18:32 +0545

    add admin routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,29 @@
 import {Routes,Route} from 'react-router-dom'
 import Home from "./pages/Home"
 import Blog from "./pages/Blog"
+import Dashboard from './pages/Admin/Dashboard'
+import Layout from './pages/Admin/Layout'
+import AddBlog from './pages/Admin/AddBlog'
+import ListBlog from './pages/Admin/ListBlog'
+import Comments from './pages/Admin/Comments'
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home/>}/>
+
         <Route path='/blog/:id' element={<Blog/>}/>
+
+
+        {/* Routes for admin page */}
+        <Route path='/admin' element={<Layout/>}>
+        <Route index element={<Dashboard/>}/>
+        <Route path='addBlog' element={<AddBlog/>}/>
+        <Route path='listBlog' element={<ListBlog/>}/>
+        <Route path='comments' element={<Comments/>}/>
+        </Route>
+
+
       </Routes>
     </div>
   )
