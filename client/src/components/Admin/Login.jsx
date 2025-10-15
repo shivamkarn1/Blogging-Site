7bece01aafc99ab2774713e3314ddfd7ae6165cb
Shivam67Code <evilsniper64@gmail.com>
2025-10-15 13:40:14 +0545

    remove unnecessary whitespace

--- client/src/components/Admin/Login.jsx
+++ client/src/components/Admin/Login.jsx
@@ -4,6 +4,7 @@ const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log('Login:', { email, password })
