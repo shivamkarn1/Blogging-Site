fa7bb61175610a4d23cdbcf0854782b9eb272848
shivamkarn <shivamkarn767@gmail.com>
2025-10-18 08:19:02 +0545

    Blog all  CRUD API's complete.

--- backend/src/controllers/blog.controller.js
+++ backend/src/controllers/blog.controller.js
@@ -5,7 +5,7 @@ import { Blog } from "../models/blog.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
-export const addBlog = asyncHandler(async (req, res) => {
+const addBlog = asyncHandler(async (req, res) => {
     // Debug consoles
     console.log("=== CREATE BLOG DEBUG ===")
     console.log("req body is : ", req.body)
@@ -42,3 +42,100 @@ export const addBlog = asyncHandler(async (req, res) => {
         .status(201)
         .json(new ApiResponse(201, { blog }, "Blog Created Successfully"))
 });
+const getAllBlog = asyncHandler(async (req, res) => {
+    try {
+        const blogs = await Blog.find({ isPublished: true })
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { blogs }, "All Blogs Fetched Successfully"))
+    } catch (error) {
+        throw new ApiError(400, "Error Fetching Blogs")
+    }
+})
+const getBlogById = asyncHandler(async (req, res) => {
+    const { blogId } = req.params;
+    if (!blogId) {
+        throw new ApiError(404, "Blog Id not Found")
+    }
+    const blog = await Blog.findById(blogId)
+    if (!blog) {
+        throw new ApiError(404, "Blog Post not found/ Doesn't Exists")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            blog,
+            "Blog post Fetched Successfully"
+        )
+        )
+})
+
+const deleteBlogById = asyncHandler(async (req, res) => {
+    const { blogId } = req.body;
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+        throw new ApiError(404, "Blog not found/ Doesn't Exits")
+    }
+    await Blog.findByIdAndDelete(blogId);
+    return res.json(new ApiResponse(200, {}, "Blog Deleted Successfully"))
+})
+
+const togglePublish = asyncHandler(async (req, res) => {
+    const { blogId } = req.body;
+    const blog = await Blog.findById(blogId).select("-description");
+    if (!blog) {
+        throw new ApiError(404, "No blog found with such Blog Id")
+    }
+    blog.isPublished = !blog.isPublished;
+    await blog.save({ validateBeforeSave: false });
+    return res.status(200).json(new ApiResponse(200, { blog }, "Blog Publish status toggled Successfully"))
+
+})
+
+const updateBlog = asyncHandler(async (req, res) => {
+    const { blogId } = req.params;
+    const { title, subTitle, description, category } = req.body;
+
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+        throw new ApiError(404, "No blog found with such Blog Id")
+    }
+    let featuredImageUrl = blog.featuredImage;
+    if (req.file) {
+        const featuredImageLocalPath = req.file.path;
+        const featuredImage = await uploadOnCloudinary(featuredImageLocalPath);
+        featuredImageUrl = featuredImage?.url || blog.featuredImage;
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+        blogId,
+        {
+            title: title || blog.title,
+            subTitle: subTitle || blog.subTitle,
+            description: description || blog.description,
+            featuredImage: featuredImageUrl
+        }, { new: true }
+    ).select("-token")
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, updatedBlog, "Blog updated SuccessFully")
+        )
+
+
+})
+
+
+
+
+export {
+    addBlog,
+    getAllBlog,
+    getBlogById,
+    deleteBlogById,
+    togglePublish,
+    updateBlog
+}
--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -1,9 +1,26 @@
 import express,{Router} from 'express'
-import { addBlog } from '../controllers/blog.controller.js';
+import { 
+    addBlog, 
+    deleteBlogById, 
+    getAllBlog, 
+    getBlogById, 
+    togglePublish, 
+    updateBlog 
+} from '../controllers/blog.controller.js';
 import { upload } from '../middleware/multer.middleware.js';
 import {auth} from "../middleware/auth.middleware.js"
+
+
 const blogRouter = Router();
 
+// NON PROTECTED ROUTES
+blogRouter.get('/all',getAllBlog)
+blogRouter.get("/:blogId",getBlogById)
+
+// PROTECTED ROUTES
 blogRouter.post("/add",upload.single('image'),auth,addBlog)
+blogRouter.post('/delete',auth,deleteBlogById)
+blogRouter.post("/toggle-publish",auth,togglePublish)
+blogRouter.patch("/:blogId", auth, upload.single('image'), updateBlog)
 
 export default blogRouter;
