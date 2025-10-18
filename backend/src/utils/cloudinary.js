b99843567ebf496f47db883e18be7fea89eb192b
shivamkarn <shivamkarn767@gmail.com>
2025-10-18 07:20:08 +0545

    Added cloudinary

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+})
+
+const uploadOnCloudinary = async(localFilePath) =>{
+    try {
+        if(!localFilePath){
+            console.log("No File path provided to upload on cloudinary")
+            return null;
+        }
+
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+
+        // Delete file then
+        fs.unlinkSync(localFilePath)
+        return response;
+    } catch (error) {
+        if(localFilePath && fs.existsSync(localFilePath)){
+            fs.unlinkSync(localFilePath)
+        }
+        console.log("Cloudinary Upload Error ; ",error)
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
