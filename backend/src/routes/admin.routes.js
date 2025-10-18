43f8f68451bc7d0832c582fc4b16e00e8b1880f3
shivamkarn <shivamkarn767@gmail.com>
2025-10-18 12:19:20 +0545

    Added the API routes.

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,8 +1,24 @@
 import express from "express";
-import { adminLogin } from "../controllers/admin.controller.js";
+import { 
+    adminLogin, 
+    approveCommentById, 
+    deleteCommentById, 
+    getAllBlogsAdmin, 
+    getAllComments, 
+    getDashboardData 
+} from "../controllers/admin.controller.js";
+import { auth } from "../middleware/auth.middleware.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login",adminLogin)
+
+// auth Routes
+adminRouter.get("/comments",auth,getAllComments)
+adminRouter.get("/blogs",auth,getAllBlogsAdmin)
+adminRouter.post("/delete-comment",auth,deleteCommentById)
+adminRouter.post("/approve-comment",auth,approveCommentById)
+adminRouter.get("/dashboard",auth,getDashboardData)
+
 
 export default adminRouter;
--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -1,9 +1,11 @@
 import express,{Router} from 'express'
 import { 
     addBlog, 
+    addComment, 
     deleteBlogById, 
     getAllBlog, 
     getBlogById, 
+    getBlogComments, 
     togglePublish, 
     updateBlog 
 } from '../controllers/blog.controller.js';
@@ -17,10 +19,14 @@ const blogRouter = Router();
 blogRouter.get('/all',getAllBlog)
 blogRouter.get("/:blogId",getBlogById)
 
+blogRouter.post('/add-comment',addComment)
+blogRouter.get("/comments/:blogId",getBlogComments)
+
 // PROTECTED ROUTES
 blogRouter.post("/add",upload.single('image'),auth,addBlog)
 blogRouter.post('/delete',auth,deleteBlogById)
 blogRouter.post("/toggle-publish",auth,togglePublish)
 blogRouter.patch("/:blogId", auth, upload.single('image'), updateBlog)
+
 
 export default blogRouter;
