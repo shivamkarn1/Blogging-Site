246fc7a5f6a7ed51edc8ebe5c848d76dae450b21
shivamkarn <shivamkarn767@gmail.com>
2025-10-19 16:09:33 +0545

    Refactor blog model for improved readability and structure

--- backend/src/models/blog.model.js
+++ backend/src/models/blog.model.js
@@ -1,31 +1,44 @@
-import mongoose,{Schema} from 'mongoose';
+import mongoose, { Schema } from "mongoose";
 
 const blogSchema = new Schema(
-    {
-        title:{
-            type:String,
-            required:true,
-        },
-        subTitle:{
-            type:String,
-        },
-        description:{
-            type:String,required:true
-        },
-        category:{
-            type:String,
-            required:true
-        },
-        featuredImage:{
-            type:String
-        },
-        isPublished:{
-            type:Boolean,
-            required:true
-        }
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    {timestamps:true}
-)
+    subTitle: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    featuredImage: {
+      type: String,
+    },
+    isPublished: {
+      type: Boolean,
+      required: true,
+    },
+    authorType: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    authorEmail: {
+      type: String,
+      required: true,
+    },
+    authorName: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export const Blog = mongoose.model("Blog", blogSchema);
-
